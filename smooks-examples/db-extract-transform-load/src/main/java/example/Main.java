02fb79fb5682e5a5fb7acea74e50a9d783ca0f9c
Marek Prochera <marek.prochera@gmail.com>
2016-08-18 11:29:25 +0200

    Fixed test contract

--- smooks-examples/db-extract-transform-load/src/main/java/example/Main.java
+++ smooks-examples/db-extract-transform-load/src/main/java/example/Main.java
@@ -78,6 +78,8 @@ public class Main {
 
     protected void runSmooksTransform() throws IOException, SAXException, SmooksException {
     	Locale defaultLocale = Locale.getDefault();
+    	Locale.setDefault(new Locale("en", "IE"));
+    	TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     	
     	Smooks smooks = new Smooks("./smooks-configs/smooks-config.xml");
 
